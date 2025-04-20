12bf3c91c06b5ef0b49d49309c97a34e4cf86b1d
Giuseppe De Palma <dpl.gsp@tutanota.com>
2025-04-20 13:43:17 +0200

    Setup jsdom

--- assets/ava.config.js
+++ assets/ava.config.js
@@ -1,9 +1,10 @@
 export default {
+    require: [
+        'jsdom-global/register.js'
+    ],
     typescript: {
-        rewritePaths: {
-            "src/": "build/"
-        },
+        rewritePaths: { "src/": "build/" },
         compile: false
     },
     nodeArguments: ['--import=tsimp/import']
-}
+};
