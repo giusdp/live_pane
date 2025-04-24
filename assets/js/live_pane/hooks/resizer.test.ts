13ff9e3e9dfd9e14cd02d26cc3b95aea0abe698d
Giuseppe De Palma <dpl.gsp@tutanota.com>
2025-04-24 12:01:39 +0200

    Refactor resize events setup

--- assets/js/live_pane/hooks/resizer.test.ts
+++ assets/js/live_pane/hooks/resizer.test.ts
@@ -57,7 +57,7 @@ test('Mounting resizer registers it to the resizerInstances map', t => {
   t.is(resizerData!.disabled.get(), false);
   t.is(resizerData!.isDragging.get(), false);
   t.not(resizerData!.resizeHandlerCallback, null);
-  t.is(resizerData!.unsubs.length, 3);
+  t.is(resizerData!.unsubs.length, 1);
 });
 
 test('Resizer gets removed on destroy', t => {
