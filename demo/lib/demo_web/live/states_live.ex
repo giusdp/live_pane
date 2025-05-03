48ec077068fced0542d70a0234039ecf4df61980
Giuseppe De Palma <dpl.gsp@tutanota.com>
2025-05-03 11:17:03 +0200

    Setup new pane states example

---
+++ demo/lib/demo_web/live/states_live.ex
@@ -0,0 +1,23 @@
+defmodule DemoWeb.StatesLive do
+  use DemoWeb, :live_view
+
+  alias Phoenix.LiveView.JS
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, collapsed: false)}
+  end
+
+  def handle_event("toggle", _, socket) do
+    collapsed = socket.assigns.collapsed
+    ev = if collapsed, do: "expand", else: "collapse"
+
+    socket =
+      socket
+      |> update(:collapsed, &(!&1))
+      |> push_event(ev, %{
+        pane_id: "demo_pane_1"
+      })
+
+    {:noreply, socket}
+  end
+end
